e5e90d4e523f8351131ff5fed623f6a7a5c6d90e
Matthew Berthoud <mwberthoud@wm.edu>
2025-03-04 13:12:51 -0500

    documented

--- Add.go
+++ Add.go
@@ -1,5 +1,9 @@
+// Package ch10_exercises_module is a simple package with an add function.
+// It is part of the Learning Go textbook exercises.
 package ch10_exercises_module
 
+// Add takes two inputs x and y, and returns the sum, as per
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(x, y int) int {
 	return x + y
 }
